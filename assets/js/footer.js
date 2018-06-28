81294a3580a22431777bca186fb9034651bcd98e
rahuljain5 <rjain.rahul5@gmail.com>
2018-06-28 18:05:41 +0530

    Moved JS and added Footer File

---
+++ assets/js/footer.js
@@ -0,0 +1,13 @@
+var footer = `<div class="footer container-fluid" style="vertical-align:bottom; width:99%; ">
+		<a href="https://github.com/rahuljain5">
+			<img src="./assets/images/github_icon.png" alt="github logo" height="40px" width="40px"></img>
+		</a>
+		<a href="profile.html">
+			<img src="./assets/images/hireme_icon.png" alt="Hireme logo" height="40px" width="40px"></img>
+		</a>
+		<h4 style="float:right;">By
+			<b>Rahul Jain
+				<b>
+		</h4>
+    </div>`
+document.write(footer);
